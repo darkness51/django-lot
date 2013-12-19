0abe7962d247a5116ed086dd9d4a168c6835f1b4
Carlos Aguilar <caguilar@dwdandsolutions.com>
2013-12-19 10:44:17 -0600

    Changin the way to get uuid and hook_url from GET params

--- lot/views.py
+++ lot/views.py
@@ -8,8 +8,17 @@ from django.contrib.auth import authenticate, login
 
 from .models import LOT
 
+
 class LOTLogin(View):
-    def get(self, request, uuid):
+
+    def get(self, request, *args, **kwargs):
+        uuid = kwargs['uuid']
+        hook_url = request.GET.get('hook_url')
+        if hook_url:
+            request.session['hook_url'] = hook_url
+        else:
+            raise Exception("Invalid hook_url param")
+
         next_url = request.GET.get('next', '/')
         lot = get_object_or_404(LOT, uuid=uuid)
         if not lot.verify():
